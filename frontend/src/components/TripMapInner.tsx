33a40f1fdd79cedd381ea9e4795221bdcddc02c3
Christyan Simbolon <sambungdata@gmail.com>
2026-03-18 16:36:42 +0700

    feat-frontend: update TripMapInner

--- frontend/src/components/TripMapInner.tsx
+++ frontend/src/components/TripMapInner.tsx
@@ -78,3 +78,14 @@ export function TripMapInner({
     </div>
   );
 }
+
+
+// import L from "leaflet";
+
+// delete L.Icon.Default.prototype._getIconUrl;
+
+// L.Icon.Default.mergeOptions({
+//   iconRetinaUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon-2x.png",
+//   iconUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-icon.png",
+//   shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+// });
